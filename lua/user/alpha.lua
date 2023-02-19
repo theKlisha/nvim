64b76537bf3021005c8ec684167e0e7f2470db50
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-02-20 00:10:42 +0100

    feat: update alpha config

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -3,31 +3,81 @@ if not status_ok then
 	return
 end
 
-local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-	[[                .                        ]],
-	[[               ":"                       ]],
-	[[             ___:____     |"\/"|         ]],
-	[[           ,'        `.    \  /          ]],
-	[[           |  O        \___/  |          ]],
-	[[^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^]],
-	[[     ._  _ ___ _____   _____ __  __      ]],
-	[[     | \| | __/ _ \ \ / /_ _|  \/  |     ]],
-	[[     | .` | _| (_) \ V / | || |\/| |     ]],
-	[[     |_|\_|___\___/ \_/ |___|_|  |_|     ]],
-}
-dashboard.section.buttons.val = {
-	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-	dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-	dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+local banner = {
+	[[                  .                          ]],
+	[[                 ":"                         ]],
+	[[               ___:____     |"\/"|           ]],
+	[[             ,'        `.    \  /            ]],
+	[[             |  O        \___/ /             ]],
+	[[^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^]],
+	[[        _  _ ___ _____   _____ __  __        ]],
+	[[       | \| | __/ _ \ \ / /_ _|  \/  |       ]],
+	[[       | .` | _| (_) \ V / | || |\/| |       ]],
+	[[       |_|\_|___\___/ \_/ |___|_|  |_|       ]],
 }
 
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
+local function button(sc, txt, keybind)
+	local opts = {
+		position = "center",
+		cursor = 5,
+		width = 29,
+		align_shortcut = "right",
+		hl_shortcut = "Keyword",
+		shortcut = sc,
+		keymap = { "n", sc, keybind, { noremap = true, silent = true, nowait = true } },
+	}
 
-dashboard.opts.opts.noautocmd = true
-alpha.setup(dashboard.opts)
+	local function on_press()
+		local key = vim.api.nvim_replace_termcodes(keybind, true, false, true)
+		vim.api.nvim_feedkeys(key, "t", false)
+	end
+
+	return {
+		type = "button",
+		val = txt,
+		on_press = on_press,
+		opts = opts,
+	}
+end
+
+local config = {
+	layout = {
+		{ type = "padding", val = 5 },
+		{
+			type = "text",
+			val = banner,
+			opts = {
+				position = "center",
+				hl = "Type",
+			},
+		},
+		{ type = "padding", val = 1 },
+		{
+			type = "text",
+			val = "- Klisha -",
+			opts = {
+				position = "center",
+				hl = "LineNr",
+			},
+		},
+		{ type = "padding", val = 2 },
+		{
+			type = "group",
+			val = {
+				button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+				button("e", " " .. " New file", ":ene <CR>"),
+				button("p", " " .. " Find project", ":Telescope projects <CR>"),
+				button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				button("t", "󱎸 " .. " Find text", ":Telescope live_grep <CR>"),
+				button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+			},
+			opts = {
+				spacing = 1,
+			},
+		},
+		{ type = "padding", val = 2 },
+		button("q", " " .. " Quit", ":qa <CR>"),
+	},
+}
+
+alpha.setup(config)

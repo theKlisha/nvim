cc74fbeb9e9da112dda831e7383fe8767ce10753
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-02-19 14:57:18 +0100

    style: format whole codebase

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,12 +9,12 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier,
-    formatting.stylua,
-    formatting.google_java_format,
-    diagnostics.flake8,
-  },
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier,
+		formatting.stylua,
+		formatting.google_java_format,
+		diagnostics.flake8,
+	},
+})

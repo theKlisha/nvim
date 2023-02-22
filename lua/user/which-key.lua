409bc3523861d35bad5cd13a99368efa4046b2cd
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-02-22 23:05:20 +0100

    refactor: remove old config

--- lua/user/which-key.lua
+++
@@ -1,9 +0,0 @@
-local which_key_ok, which_key = pcall(require, "which-key")
-if not which_key_ok then
-	return
-end
-
-which_key.setup({
-	-- your configuration comes here
-	-- or leave it empty to use the default settings
-})

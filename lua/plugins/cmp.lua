0cdc11a8f0cdc564f1f00168d985f642cac1283a
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-02-22 23:06:33 +0100

    feat: add lspconfig

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,83 @@
+local check_backspace = function()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		event = "InsertEnter",
+		opts = function()
+			local cmp = require("cmp")
+			return {
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+				formatting = {
+					format = function(_, item)
+						local icons = {
+							Text = "",
+							Method = "",
+							Function = "",
+							Constructor = "",
+							Field = "",
+							Variable = "",
+							Class = "",
+							Interface = "",
+							Module = "",
+							Property = "",
+							Unit = "",
+							Value = "",
+							Enum = "",
+							Keyword = "",
+							Snippet = "",
+							Color = "",
+							File = "",
+							Reference = "",
+							Folder = "",
+							EnumMember = "",
+							Constant = "",
+							Struct = "",
+							Event = "",
+							Operator = "",
+							TypeParameter = "",
+						}
+
+						if icons[item.kind] then
+							item.kind = icons[item.kind] .. " " .. item.kind
+						end
+						return item
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				experimental = {
+					ghost_text = true,
+				},
+			}
+		end,
+	},
+}

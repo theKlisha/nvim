3113bd51672084631e5c8d734f03bb2987b58c7f
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-02-22 23:07:28 +0100

    feat: add bufferline

---
+++ lua/plugins/bufferemove.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"echasnovski/mini.bufremove",
+        -- stylua: ignore
+        keys = {
+            { "<leader>q", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+            { "<leader>Q", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+        },
+	},
+}

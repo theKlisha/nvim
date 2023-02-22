c854922d28cd1e384536040820602f4830fd8b30
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-02-22 23:05:50 +0100

    feat: init lazy.nvim

--- init.lua
+++ init.lua
@@ -1,22 +1,18 @@
-require("user.impatient")
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-require("user.autocommands")
-require("user.colorscheme")
-require("user.cmp")
-require("user.telescope")
-require("user.gitsigns")
-require("user.treesitter")
-require("user.autopairs")
-require("user.comment")
-require("user.nvim-tree")
-require("user.bufferline")
-require("user.lualine")
-require("user.toggleterm")
-require("user.illuminate")
-require("user.indentline")
-require("user.alpha")
-require("user.which-key")
-require("user.lsp")
-require("user.dap")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("options")
+
+require("lazy").setup("plugins", {
+	ui = { border = "rounded" },
+})

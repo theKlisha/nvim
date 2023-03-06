643511c491617b1e7d8b09dda44ffbf2fea6812e
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-03-06 01:50:44 +0100

    fix: null-ls config

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,8 +6,10 @@ return {
 		return {
 			sources = {
 				nls.builtins.formatting.prettierd,
+                nls.builtins.code_actions.eslint_d,
 				nls.builtins.formatting.stylua,
 				nls.builtins.diagnostics.flake8,
+                nls.builtins.code_actions.gitsigns,
 			},
 		}
 	end,

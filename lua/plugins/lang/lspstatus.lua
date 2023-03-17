48c8e239d03ee4dd48401bd66ce38f7dba55c676
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-03-17 17:36:23 +0100

    feat: update lualine config

--- lua/plugins/lang/lspstatus.lua
+++ lua/plugins/lang/lspstatus.lua
@@ -1,6 +1,24 @@
 return {
 	"nvim-lua/lsp-status.nvim",
-	config = function()
+	opts = {
+		kind_labels = {},
+		current_function = true,
+		show_filename = true,
+		diagnostics = true,
+		indicator_separator = " ",
+		component_separator = " ",
+		indicator_errors = "",
+		indicator_warnings = "󰀦",
+		indicator_info = "󰋼",
+		indicator_hint = "󰌵",
+		indicator_ok = "󰆦",
+		spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
+		status_symbol = "",
+		select_symbol = nil,
+		update_interval = 100,
+	},
+	config = function(_, opts)
+        require("lsp-status").config(opts)
 		require("lsp-status").register_progress()
 	end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,47 @@
 return {
 	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-lua/lsp-status.nvim",
+	},
 	event = "VimEnter",
-	config = function()
-		require("lualine").setup({
-			sections = {
-				lualine_c = {
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						-- color = { fg = "#ff9e64" },
-					},
-				},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "│", right = "│" },
+			section_separators = { left = "▒", right = "▒" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
 			},
-		})
-	end,
+			ignore_focus = {},
+			always_divide_middle = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = {},
+			lualine_y = { "filetype", "require('lsp-status').status()" },
+			lualine_z = { "progress", "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	},
 }

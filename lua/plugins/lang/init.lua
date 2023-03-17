32a08d04b0a7e6bd6763ae2a6f0d9acb625a134b
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-03-17 16:13:56 +0100

    feat: java support via jdtls

---
+++ lua/plugins/lang/init.lua
@@ -0,0 +1,8 @@
+return {
+	require("plugins.lang.cmp"),
+	require("plugins.lang.jdtls"),
+	require("plugins.lang.lspconfig"),
+	require("plugins.lang.lspstatus"),
+	require("plugins.lang.mason"),
+	require("plugins.lang.treesitter"),
+}

222aa188346b26d51cef473a21c1c530eae4f1f0
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-05-05 14:36:33 +0200

    feat: add java format config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,16 @@
 local config = {
 	cmd = { "jdtls" },
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+
+	settings = {
+		java = {
+			format = {
+				settings = {
+					url = "/Users/grzegorz/.config/nvim/ext/javaFormat.xml",
+				},
+			},
+		},
+	},
 }
 
 require("jdtls").start_or_attach(config)

9cf1f834e62e1defb96b913daa5557f70d28bbf6
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-05-05 14:10:00 +0200

    feat: automatically change colorscheme based on terminal emulator

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,13 @@ return {
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme melange]])
+		if vim.env.TERM_PROGRAM ~= "Apple_Terminal" then
+			vim.opt.termguicolors = true
+			vim.cmd([[colorscheme melange]])
+		else
+			vim.opt.termguicolors = false
+			local palette = require("mini.base16").mini_palette("#292929", "#c4bc76", 50)
+			require("mini.base16").setup({ palette = palette })
+		end
 	end,
 }

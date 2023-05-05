55c3412ff86bddc1af4b6fc9b01df81c8b4826e4
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-05-05 14:24:29 +0200

    feat: add visual-multi cursor plugin

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+	"github/copilot.vim",
+	evemt = "InsertEnter",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+		vim.g.copilot_filetypes = {
+			["qf"] = false,
+			["telescope"] = false,
+		}
+	end,
+}
---
+++ lua/plugins/toggleterm/init.lua
@@ -0,0 +1,60 @@
+return {
+	"akinsho/toggleterm.nvim",
+	cmd = {
+		"OolongToggleShell",
+		"OolongToggleLazygit",
+	},
+	opts = {
+		size = function(term)
+			if term.direction == "horizontal" then
+				return vim.o.lines * 0.5
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.4
+			end
+		end,
+		direction = "float",
+		float_opts = { border = "curved" },
+		hidden = true,
+		highlights = {
+			NormalFloat = {
+				link = "FloatNormal",
+			},
+			FloatBorder = {
+				link = "FloatBorder",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+
+		local shell = require("toggleterm.terminal").Terminal:new({
+			shell = vim.o.shell,
+			start_in_insert = true,
+			on_open = function(term)
+				-- vim.cmd("startinsert")
+				term:set_mode("i")
+				vim.api.nvim_buf_set_keymap(0, "t", [[<Esc>]], [[<C-\><C-n>]], { noremap = true, silent = true })
+			end,
+		})
+
+		local lazygit = require("toggleterm.terminal").Terminal:new({
+			cmd = "lazygit -ucd ~/.config/nvim/lua/plugins/toggleterm/lazygit/",
+		})
+
+		vim.api.nvim_create_user_command("OolongToggleShell", function()
+			if lazygit:is_open() then
+				lazygit:close()
+			else
+				shell:toggle()
+			end
+		end, {})
+
+		vim.api.nvim_create_user_command("OolongToggleLazygit", function()
+			if shell:is_open() then
+				shell:close()
+			else
+				lazygit:toggle()
+			end
+		end, {})
+	end,
+}

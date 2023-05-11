6f3b0b14c5f9538c13b35460fc2ebaef825c675b
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2023-05-11 16:08:57 +0200

    feat: set theme to oolong.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-	"savq/melange-nvim",
+	"Grzdaczek/oolong.nvim",
+	dependencies = { "echasnovski/mini.base16" },
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		if vim.env.TERM_PROGRAM ~= "Apple_Terminal" then
 			vim.opt.termguicolors = true
-			vim.cmd([[colorscheme melange]])
+			require("oolong").setup()
 		else
 			vim.opt.termguicolors = false
 			local palette = require("mini.base16").mini_palette("#292929", "#c4bc76", 50)

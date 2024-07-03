4f1dba6fdf1f10d7e5866b370646ff97cf5a2a39
Grzegorz Janysek <grzegorz.janysek@codewise.com>
2024-07-03 18:00:17 +0200

    chore: update copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,8 +2,10 @@ return {
 	"github/copilot.vim",
 	event = "InsertEnter",
 	config = function()
-		vim.g.copilot_no_tab_map = true
-		vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		-- vim.g.copilot_no_tab_map = true
+		-- vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		--
+		-- vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<C-n>")', { silent = true, expr = true })
 
 		vim.g.copilot_filetypes = {
 			["qf"] = false,

222533e56a2c130f76969a81bdc5950c8fd6ed2e
Grzegorz Janysek <gjanysek@qualtrics.com>
2025-12-05 14:07:35 +0100

    feat: add test case plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -29,5 +29,33 @@ return {
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
 		opts = {},
-	}
+	},
+	{
+		"johmsalas/text-case.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("textcase").setup({})
+			require("telescope").load_extension("textcase")
+		end,
+		keys = {
+			"<leader>c", -- Default invocation prefix
+			{ "<leader>c", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Change case" },
+			{ "<leader>cs", '<cmd>lua require("textcase").current_word("to_snake_case")<cr>', mode = { "n", "x" }, desc = "Change to snake case" },
+			{ "<leader>cC", '<cmd>lua require("textcase").current_word("to_constant_case")<cr>', mode = { "n", "x" }, desc = "Change to constant case" },
+			{ "<leader>cc", '<cmd>lua require("textcase").current_word("to_camel_case")<cr>', mode = { "n", "x" }, desc = "Change to camel case" },
+			{ "<leader>cp", '<cmd>lua require("textcase").current_word("to_pascal_case")<cr>', mode = { "n", "x" }, desc = "Change to pascal case" },
+		},
+		cmd = {
+			-- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+			"Subs",
+			"TextCaseOpenTelescope",
+			"TextCaseOpenTelescopeQuickChange",
+			"TextCaseOpenTelescopeLSPChange",
+			"TextCaseStartReplacingCommand",
+		},
+		-- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+		-- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+		-- available after the first executing of it or after a keymap of text-case.nvim has been used.
+		lazy = false,
+	},
 }

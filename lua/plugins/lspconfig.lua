61c5a32d178f3513cbc38b0adf73f2810dc93c81
Grzegorz Janysek <gjanysek@qualtrics.com>
2025-12-08 18:04:34 +0100

    chore: move to vim.lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,38 +1,78 @@
-return { -- LSP Configuration & Plugins
+return {
+	-- Main LSP Configuration
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for neovim
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
+		-- Automatically install LSPs and related tools to stdpath for Neovim
+		-- Mason must be loaded before its dependents so we need to set it up here.
+		-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+		{ "mason-org/mason.nvim", opts = {} },
+		"mason-org/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		-- Useful status updates for LSP.
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{ "j-hui/fidget.nvim", opts = {} },
+		{ "j-hui/fidget.nvim",    opts = {} },
 
-		-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- Allows extra capabilities provided by blink.cmp
+		"saghen/blink.cmp",
+
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
 		-- used for completion, annotations and signatures of Neovim apis
-		{ "folke/neodev.nvim", opts = {} },
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 
+		-- "Painless Java in Neovim"
 		{
 			"nvim-java/nvim-java",
-			opts = {},
 			config = function()
 				require("java").setup()
-				require("lspconfig").jdtls.setup({})
+				vim.lsp.enable("jdtls")
 			end,
 		},
 	},
 	config = function()
+		-- Brief aside: **What is LSP?**
+		--
+		-- LSP is an initialism you've probably heard, but might not understand what it is.
+		--
+		-- LSP stands for Language Server Protocol. It's a protocol that helps editors
+		-- and language tooling communicate in a standardized fashion.
+		--
+		-- In general, you have a "server" which is some tool built to understand a particular
+		-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
+		-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
+		-- processes that communicate with some "client" - in this case, Neovim!
+		--
+		-- LSP provides Neovim with features like:
+		--  - Go to definition
+		--  - Find references
+		--  - Autocompletion
+		--  - Symbol Search
+		--  - and more!
+		--
+		-- Thus, Language Servers are external tools that must be installed separately from
+		-- Neovim. This is where `mason` and related plugins come into play.
+		--
+		-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
+		-- and elegantly composed help section, `:help lsp-vs-treesitter`
+
 		--  This function gets run when an LSP attaches to a particular buffer.
 		--    That is to say, every time a new file is opened that is associated with
 		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
 		--    function will be executed to configure the current buffer
 		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
 				-- Jump to the definition of the word under your cursor.
@@ -81,45 +121,94 @@ return { -- LSP Configuration & Plugins
 					vim.lsp.buf.format({ async = true })
 				end, "Format buffer")
 
-				-- The following two autocommands are used to highlight references of the
-				-- word under your cursor when your cursor rests there for a little while.
-				--    See `:help CursorHold` for information about when this is executed
+				-- -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+				-- ---@param client vim.lsp.Client
+				-- ---@param method vim.lsp.protocol.Method
+				-- ---@param bufnr? integer some lsp support methods only in specific files
+				-- ---@return boolean
+				-- local function client_supports_method(client, method, bufnr)
+				-- 	if vim.fn.has 'nvim-0.11' == 1 then
+				-- 		return client:supports_method(method, bufnr)
+				-- 	else
+				-- 		return client.supports_method(method, { bufnr = bufnr })
+				-- 	end
+				-- end
+
+				-- -- The following two autocommands are used to highlight references of the
+				-- -- word under your cursor when your cursor rests there for a little while.
+				-- --    See `:help CursorHold` for information about when this is executed
+				-- --
+				-- -- When you move your cursor, the highlights will be cleared (the second autocommand).
+				-- local client = vim.lsp.get_client_by_id(event.data.client_id)
+				-- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+				-- 	local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+				-- 	vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+				-- 		buffer = event.buf,
+				-- 		group = highlight_augroup,
+				-- 		callback = vim.lsp.buf.document_highlight,
+				-- 	})
 				--
-				-- When you move your cursor, the highlights will be cleared (the second autocommand).
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.server_capabilities.documentHighlightProvider then
-					-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					-- 	buffer = event.buf,
-					-- 	callback = vim.lsp.buf.document_highlight,
-					-- })
-					--
-					-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					-- 	buffer = event.buf,
-					-- 	callback = vim.lsp.buf.clear_references,
-					-- })
-				end
+				-- 	vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+				-- 		buffer = event.buf,
+				-- 		group = highlight_augroup,
+				-- 		callback = vim.lsp.buf.clear_references,
+				-- 	})
+				--
+				-- 	vim.api.nvim_create_autocmd('LspDetach', {
+				-- 		group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+				-- 		callback = function(event2)
+				-- 			vim.lsp.buf.clear_references()
+				-- 			vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+				-- 		end,
+				-- 	})
+				-- end
+
+				-- -- The following code creates a keymap to toggle inlay hints in your
+				-- -- code, if the language server you are using supports them
+				-- --
+				-- -- This may be unwanted, since they displace some of your code
+				-- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+				-- 	map('<leader>th', function()
+				-- 		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+				-- 	end, '[T]oggle Inlay [H]ints')
+				-- end
 			end,
 		})
 
-		-- Ensure the servers and tools are installed
-		--  To check the current status of installed tools and/or manually install
-		--  other tools, you can run
-		--    :Mason
-		--
-		--  You can press `g?` for help in this menu
-		require("mason").setup({
-			-- Set border style for Mason UI
-			ui = { border = "rounded" },
+		-- Diagnostic Config
+		-- See :help vim.diagnostic.Opts
+		vim.diagnostic.config({
+			severity_sort = true,
+			float = { border = "rounded", source = "if_many" },
+			underline = { severity = vim.diagnostic.severity.ERROR },
+			signs = vim.g.have_nerd_font and {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = "󰋽 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+				},
+			} or {},
+			virtual_text = {
+				source = "if_many",
+				spacing = 2,
+				format = function(diagnostic)
+					local diagnostic_message = {
+						[vim.diagnostic.severity.ERROR] = diagnostic.message,
+						[vim.diagnostic.severity.WARN] = diagnostic.message,
+						[vim.diagnostic.severity.INFO] = diagnostic.message,
+						[vim.diagnostic.severity.HINT] = diagnostic.message,
+					}
+					return diagnostic_message[diagnostic.severity]
+				end,
+			},
 		})
 
-		local mason_registry = require("mason-registry")
-
 		-- LSP servers and clients are able to communicate to each other what features they support.
-		--  By default, Neovim doesn't support everything that is in the LSP Specification.
-		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		--  By default, Neovim doesn't support everything that is in the LSP specification.
+		--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+		--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -145,14 +234,14 @@ return { -- LSP Configuration & Plugins
 			rust_analyzer = {},
 			templ = {},
 
-			volar = {
-				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-				init_options = {
-					vue = {
-						hybridMode = false,
-					},
-				},
-			},
+			-- volar = {
+			-- 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			-- 	init_options = {
+			-- 		vue = {
+			-- 			hybridMode = false,
+			-- 		},
+			-- 	},
+			-- },
 
 			htmx = {
 				filetypes = { "html", "templ" },
@@ -208,31 +297,43 @@ return { -- LSP Configuration & Plugins
 			},
 		}
 
-		-- Add custom filetypes
-		vim.filetype.add({ extension = { templ = "templ" } })
-
-		-- Automatically install the servers above
-		require("mason-tool-installer").setup({ vim.tbl_keys(servers) })
+		-- Ensure the servers and tools above are installed
+		--
+		-- To check the current status of installed tools and/or manually install
+		-- other tools, you can run
+		--    :Mason
+		--
+		-- You can press `g?` for help in this menu.
+		--
+		-- `mason` had to be setup earlier: to configure its options see the
+		-- `dependencies` table for `nvim-lspconfig` above.
+		--
+		-- You can add other tools here that you want Mason to install
+		-- for you, so that they are available from within Neovim.
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua", -- Used to format Lua code
+		})
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
+			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+			automatic_installation = false,
 			handlers = {
 				function(server_name)
 					local server = servers[server_name] or {}
 					-- This handles overriding only values explicitly passed
 					-- by the server configuration above. Useful when disabling
-					-- certain features of an LSP (for example, turning off formatting for tsserver)
+					-- certain features of an LSP (for example, turning off formatting for ts_ls)
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 					require("lspconfig")[server_name].setup(server)
 				end,
 			},
 		})
 
-		-- Set the border style for LSP windows
-		require("lspconfig.ui.windows").default_options.border = "rounded"
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help,
-			{ border = "rounded" })
-		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
-			{ virtual_text = false })
+		require("mason").setup({
+			-- Set border style for Mason UI
+			ui = { border = "rounded" },
+		})
 	end,
 }

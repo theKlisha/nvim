1554eae3fd0f97d2dd81efb969c42642d654014e
Grzegorz Janysek <gjanysek@qualtrics.com>
2026-02-23 13:41:24 +0100

    fix: remove deprecated config options

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,3 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
 })
-
-require("comment").setup({
-  -- enables context aware comments, for example in jsx/tsx
-  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-})

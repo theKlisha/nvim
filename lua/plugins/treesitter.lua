9409edab147f6b6ff95a6d0bd583b0445e30c72d
Grzegorz Janysek <gjanysek@qualtrics.com>
2026-03-26 14:49:55 +0100

    fix: treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 require("nvim-treesitter").setup({})
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
-  callback = function() vim.treesitter.start() end,
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
+  desc = "Try to enable tree-sitter syntax highlighting",
+  pattern = "*", -- run on *all* filetypes
+  callback = function()
+    pcall(function() vim.treesitter.start() end)
+  end,
 })

5b1590b452095b3531f290f05299da46c3e0fe52
Grzegorz Janysek <gjanysek@qualtrics.com>
2026-04-30 14:14:45 +0200

    chore: agentic config

--- lua/plugins/agentic.lua
+++ lua/plugins/agentic.lua
@@ -1,8 +1,27 @@
+local animation =
+{ "⡀", "⠄", "⠂", "⠁", "⠈", "⠐", "⠠", "⢀", "⣀", "⢄", "⢂", "⢁", "⢈", "⢐", "⢠", "⣠", "⢤", "⢢", "⢡", "⢨", "⢰", "⣰", "⢴",
+	"⢲", "⢱", "⢸", "⣸", "⢼", "⢺", "⢹", "⣹", "⢽", "⢻", "⣻", "⢿", "⣿", }
+
 require("agentic").setup({
 	-- Any ACP-compatible provider works. Built-in: "claude-agent-acp" | "gemini-acp" | "codex-acp" | "opencode-acp" | "cursor-acp" | "copilot-acp" | "auggie-acp" | "mistral-vibe-acp" | "cline-acp" | "goose-acp"
-	provider = "claude-agent-acp", -- setting the name here is all you need to get started
+	provider = "cursor-acp", -- setting the name here is all you need to get started
 	windows = {
-		position = "bottom",    -- "right", "left", or "bottom"
+		position = "bottom", -- "right", "left", or "bottom"
+	},
+	diff_preview = {
+		layout = "inline",
+	},
+	spinner_chars = {
+		generating = animation,
+		thinking = animation,
+		searching = animation,
+		busy = animation,
+	},
+	message_icons = {
+		thinking = "[Grinding]",
+		finished = "[Finished]",
+		stopped = "[Stopped]",
+		error = "[Error]",
 	},
 })
 
@@ -10,7 +29,7 @@ require("which-key").add({
 	{
 		"<leader>ll",
 		function() require("agentic").toggle() end,
-		mode = { "n", "v", "i" },
+		mode = { "n", "v" },
 		desc = "Toggle Agentic Chat"
 	},
 	{
@@ -22,7 +41,7 @@ require("which-key").add({
 	{
 		"<leader>ln",
 		function() require("agentic").new_session() end,
-		mode = { "n", "v", "i" },
+		mode = { "n", "v", },
 		desc = "New Agentic Session"
 	},
 	{
@@ -32,7 +51,7 @@ require("which-key").add({
 		end,
 		desc = "Agentic Restore session",
 		silent = true,
-		mode = { "n", "v", "i" },
+		mode = { "n", "v" },
 	},
 	{
 		"<leader>ld", -- ai Diagnostics

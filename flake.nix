ae47303c6cc54e9a3f01a903dd8b69ed51112f0b
theKlisha <theKlisha>
2026-05-30 21:23:53 +0200

    chore: update inputs

--- flake.nix
+++ flake.nix
@@ -17,7 +17,11 @@
         nixpkgs.lib.genAttrs lib.systems.flakeExposed (
           system:
           f rec {
-            pkgs = import nixpkgs { inherit system; };
+            pkgs = import nixpkgs {
+              inherit system;
+              # https://github.com/johmsalas/text-case.nvim/issues/194
+              config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "text-case.nvim" ];
+            };
             neovim = pkgs.callPackage ./default.nix { };
           }
         );

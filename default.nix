925b2ca24964a330fd9df5ed62e1d77a950fae60
Grzegorz Janysek <gjanysek@qualtrics.com>
2026-09-01 17:44:32 +0200

    chore: remove deprecated makeNeovimConfig

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 {
   lib,
   neovim-unwrapped,
-  neovimUtils,
   stdenv,
   wrapNeovimUnstable,
   callPackage,
@@ -33,7 +32,7 @@ let
     '';
   };
 
-  customLuaRC = ''
+  luaRcContent = ''
     -- prepend lua directory
     vim.opt.rtp:prepend('${luaDir}')
   ''
@@ -42,41 +41,39 @@ let
 
   plugins = callPackage ./plugins.nix { };
 
-  extraConfig = {
-    wrapperArgs = [
-      "--set"
-      "JDTLS_VSCJAVA_DEBUG"
-      # https://codeberg.org/mfussenegger/nvim-jdtls#java-debug-bundle-configuration
-      "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/"
-    ]
-    ++ [
-      "--prefix"
-      "JDTLS_JVM_ARGS"
-      " "
-      "-javaagent:${lombok}/share/java/lombok.jar"
-    ]
-    ++ [
-      "--suffix"
-      "PATH"
-      ":"
-      (lib.makeBinPath [
-        fzf # used by telescope-fzy-native-nvim
-        git
-        lazygit # git tui
+  wrapperArgs = [
+    "--set"
+    "JDTLS_VSCJAVA_DEBUG"
+    # https://codeberg.org/mfussenegger/nvim-jdtls#java-debug-bundle-configuration
+    "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/"
+  ]
+  ++ [
+    "--prefix"
+    "JDTLS_JVM_ARGS"
+    " "
+    "-javaagent:${lombok}/share/java/lombok.jar"
+  ]
+  ++ [
+    "--suffix"
+    "PATH"
+    ":"
+    (lib.makeBinPath [
+      fzf # used by telescope-fzy-native-nvim
+      git
+      lazygit # git tui
 
-        gopls # Go language server: https://go.dev/gopls
-        jdt-language-server # Java language server: https://github.com/eclipse-jdtls/eclipse.jdt.ls
-        lua-language-server # Lua language server: https://github.com/LuaLS/lua-language-server
-        nil # Nix language server: https://github.com/oxalica/nil
-        sleek # small, fast and deterministic SQL formatter
-        typescript-go # TS language server: https://github.com/microsoft/typescript-go
-      ])
-    ];
-  };
+      gopls # Go language server: https://go.dev/gopls
+      jdt-language-server # Java language server: https://github.com/eclipse-jdtls/eclipse.jdt.ls
+      lua-language-server # Lua language server: https://github.com/LuaLS/lua-language-server
+      nil # Nix language server: https://github.com/oxalica/nil
+      sleek # small, fast and deterministic SQL formatter
+      typescript-go # TS language server: https://github.com/microsoft/typescript-go
+    ])
+  ];
 
-  neovimConfig = neovimUtils.makeNeovimConfig {
-    inherit customLuaRC;
-    inherit plugins;
-  };
 in
-wrapNeovimUnstable neovim-unwrapped (neovimConfig // extraConfig)
+wrapNeovimUnstable neovim-unwrapped ({
+  inherit luaRcContent;
+  inherit plugins;
+  inherit wrapperArgs;
+})
